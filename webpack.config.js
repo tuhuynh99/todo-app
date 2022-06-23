efd3454ea2a7cbaa1acd62b8b2ca6e558a42a345
tuhuynh99 <hctu.study@gmail.com>
2022-06-23 15:14:12 +0700

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+
+
+module.exports = {
+  entry: './assets/index.js',  // path to our input file
+  output: {
+    filename: 'index-bundle.js',  // output bundle file name
+    path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+  module: {
+    rules: [
+        // this is for react js
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    //   this is for vuejs 
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ]
+  }
+};
