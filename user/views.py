fe75c4db296b7710b8e142fc795b99b0c76d9be3
tuhuynh99 <hctu.study@gmail.com>
2022-07-01 10:56:03 +0700

    simple authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from allauth.account.views import SignupView
 # Create your views here.
+
+
+class UserSignupView(SignupView):
+    # change template name and path
+    template_name = 'user/custom_signup.html'
+    
+
+    
