37ac175ce814185ff55d5a202a19419cb06c8ab6
tuhuynh99 <hctu.study@gmail.com>
2022-07-04 20:31:48 +0700

    debug true

--- dummy/settings.py
+++ dummy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xv*f&a-^*4(26!i#h@klvmt%bn19-iwq&_f5$xj4_o)wyl09x9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['anotheroaktree.herokuapp.com', '0.0.0.0', '127.0.0.1', 'localhost']
 
